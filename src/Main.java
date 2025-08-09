f25aff582d912ff66d4f396da1bcee2e0a0d01b9
mateus <matheushenrique8790@outlook.com>
2025-08-08 23:00:41 -0300

    Atualização do projeto

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,50 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        Locale.setDefault(Locale.US);
+        Produto produto = new Produto();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Conferencia de Estoque:");
+
+        System.out.print("nome do produto: ");
+        produto.produto = sc.nextLine();
+        System.out.print("preço: ");
+        produto.preco = sc.nextDouble();
+        System.out.print("quantidade: ");
+        produto.quantidade = sc.nextInt();
+        System.out.println();
+
+        System.out.println("Estoque de Hoje: " + produto);
+
+        System.out.println("Digite a quantidade que será adicionada: ");
+        int quantidade = sc.nextInt();
+        produto.AddProduto(quantidade);
+
+        System.out.println("Estoque Atualizado: " + produto);
+        System.out.println();
+
+        System.out.print("Deseja retirar do Estoque: ");
+        char opcao = sc.next().charAt(0);
+
+        if (opcao == 's'){
+            System.out.println("Digite a quantidade á ser retirado: ");
+            int remove = sc.nextInt();
+            produto.RemoveProduto(remove);
         }
+        else {
+
+        }
+        System.out.println(produto);
+
+
+
+
+        sc.close();
     }
 }
--- src/Produto.java
+++ src/Produto.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Produto {
+
+    public double preco;
+    public int quantidade;
+    public String produto;
+
+    public double TotalValorEstoque(){
+        return quantidade * preco;
+    }
+
+    public void AddProduto(int quantidade){
+       this.quantidade += quantidade;
+
+    }
+    public void RemoveProduto(int quantidade) {
+        this.quantidade -= quantidade;
+    }
+    @Override
+    public String toString() {
+        return produto + ", $ "
+                + String.format("%.2f",preco) + ", " + quantidade
+                + " quantidade, Total: "
+                + String.format("%.2f",TotalValorEstoque());
+    }
 }
